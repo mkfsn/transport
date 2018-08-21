684c286afcd3b2242988aa343f6744d5849501bb
mkfsn <p408865@gmail.com>
2018-08-21 16:01:51 +0800

    fix: build files to dist folder and then deploy it in public folder

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 pushd $DIR/public
 
+mv ../dist/* ./
 git add -A .
 git commit -m "Site updated by Travis: $(date +%Y-%m-%d\ %H:%M:%S)"
 git push origin gh-pages --quiet
