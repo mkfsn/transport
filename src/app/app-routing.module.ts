277da149c8c0fbed59e467592338ad31c2f15957
mkfsn <p408865@gmail.com>
2018-08-21 13:40:52 +0800

    feat: add routing for tra and thsr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TraComponent } from './components/tra/tra.component';
+import { ThsrComponent } from './components/thsr/thsr.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tra', component: TraComponent },
+  { path: 'thsr', component: ThsrComponent },
+  { path: '', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
