92610f5e47b5b7b1208687706a254b58641a92ba
mkfsn <p408865@gmail.com>
2018-08-21 15:28:44 +0800

    feat: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Exit with nonzero exit code if anything fails
+set -e 
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+pushd $DIR/public
+
+git add -A .
+git commit -m "Site updated by Travis: $(date +%Y-%m-%d\ %H:%M:%S)"
+git push origin gh-pages --quiet
+
+popd
